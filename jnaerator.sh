26535712d686b273ff7eb0b530b8b7b794c6cac8
Konstantin Herud <konstantin.herud@gmail.com>
2023-08-29 14:12:33 +0200

    Update jna code generation script

--- jnaerator.sh
+++ jnaerator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar jnaerator.jar \
+jnaerator \
 	-beanStructs \
 	-direct \
 	-emptyStructsAsForwardDecls \
@@ -7,9 +7,23 @@ java -jar jnaerator.jar \
 	-library llama \
 	-package de.kherud.llama.foreign \
 	-limitComments \
+	-skipDeprecated \
 	-noComments \
 	-o src/main/java \
 	 -preferJavac \
 	 -mode Directory \
 	 -runtime JNA \
+	 -f \
 	 llama.h
+# remove erroneous method signatures
+find src/main/java/de/kherud/llama/foreign \
+ 	-type f \
+ 	-name "*.java" \
+ 	-exec sed -i 's/protected List<? > getFieldOrder()/@Override\n\tprotected List<String> getFieldOrder()/g' {} +
+# remove unknown imports
+find src/main/java/de/kherud/llama/foreign \
+	-type f \
+	-name "*.java" \
+	-exec sed -i -E '/import com\.ochafik\.lang\.jnaerator\.runtime\.NativeSize(ByReference)?;/d' {} +
+# remove deprecated
+find src/main/java/de/kherud/llama/foreign -type f -name "*.java" -exec sed -i '/@Deprecated/{N; d;}' {} +
