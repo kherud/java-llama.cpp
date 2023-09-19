b5b91a0830b052056f57b5bc156985e29f31fd4e
Konstantin Herud <konstantin.herud@denkbares.com>
2023-09-19 21:23:59 +0200

    Add build script parenthesis

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 pushd ..
 mkdir -p build
 cd build
-cmake .. $@ || popd && exit 1
-cmake --build . --config Release || popd && exit 1
+cmake .. $@ || (popd && exit 1)
+cmake --build . --config Release || (popd && exit 1)
 popd
