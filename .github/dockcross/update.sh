02c54c0c012cb267074cd9317fb7853d3d36fd1b
Konstantin Herud <konstantin.herud@denkbares.com>
2024-01-04 21:22:16 +0100

    Add android dockcross images

--- .github/dockcross/update.sh
+++ .github/dockcross/update.sh
@@ -4,4 +4,8 @@
 docker run --rm dockcross/manylinux2014-x64  > ./dockcross-manylinux2014-x64
 docker run --rm dockcross/manylinux2014-x86  > ./dockcross-manylinux2014-x86
 docker run --rm dockcross/linux-arm64-lts    > ./dockcross-linux-arm64-lts
+docker run --rm dockcross/android-arm        > ./dockcross-android-arm
+docker run --rm dockcross/android-arm64      > ./dockcross-android-arm64
+docker run --rm dockcross/android-x86        > ./dockcross-android-x86
+docker run --rm dockcross/android-x86_64     > ./dockcross-android-x86_64
 chmod +x ./dockcross-*
