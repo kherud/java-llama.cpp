32540b2a6ebddd9fde1215f7aa6025826b728b51
Konstantin Herud <konstantin.herud@gmail.com>
2024-04-05 18:54:48 +0200

    Update examples

--- src/test/java/examples/InfillExample.java
+++ src/test/java/examples/InfillExample.java
@@ -1,23 +1,25 @@
 package examples;
 
+import de.kherud.llama.InferenceParameters;
 import de.kherud.llama.LlamaModel;
 import de.kherud.llama.ModelParameters;
-import de.kherud.llama.ModelResolver;
 
 public class InfillExample {
 
 	public static void main(String... args) {
-		LlamaModel.setLogger((level, message) -> System.out.print(message));
 		ModelParameters modelParams = new ModelParameters()
+				.setModelFilePath("models/codellama-7b.Q2_K.gguf")
+				.setModelUrl("https://huggingface.co/TheBloke/CodeLlama-7B-GGUF/resolve/main/codellama-7b.Q2_K.gguf")
 				.setNGpuLayers(43);
 
 		String prefix = "def remove_non_ascii(s: str) -> str:\n    \"\"\" ";
 		String suffix = "\n    return result\n";
-		String modelName = System.getProperty("model.name");
-		String modelPath = ModelResolver.getPathToModel(modelName);
-		try (LlamaModel model = new LlamaModel(modelPath, modelParams)) {
+		try (LlamaModel model = new LlamaModel(modelParams)) {
 			System.out.print(prefix);
-			for (LlamaModel.Output output : model.generate(prefix, suffix)) {
+			InferenceParameters inferParams = new InferenceParameters("")
+					.setInputPrefix(prefix)
+					.setInputSuffix(suffix);
+			for (LlamaModel.Output output : model.generate(inferParams)) {
 				System.out.print(output);
 			}
 			System.out.print(suffix);
--- src/test/java/examples/MainExample.java
+++ src/test/java/examples/MainExample.java
@@ -1,6 +1,5 @@
 package examples;
 
-import de.kherud.llama.ModelResolver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,29 +8,23 @@ import java.nio.charset.StandardCharsets;
 import de.kherud.llama.InferenceParameters;
 import de.kherud.llama.LlamaModel;
 import de.kherud.llama.ModelParameters;
+import de.kherud.llama.args.MiroStat;
 
+@SuppressWarnings("InfiniteLoopStatement")
 public class MainExample {
 
     public static void main(String... args) throws IOException {
-        LlamaModel.setLogger((level, message) -> System.out.print(message));
         ModelParameters modelParams = new ModelParameters()
+				.setModelFilePath("models/mistral-7b-instruct-v0.2.Q2_K.gguf")
+				.setModelUrl("https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q2_K.gguf")
                 .setNGpuLayers(43);
-        InferenceParameters inferParams = new InferenceParameters()
-                .setTemperature(0.7f)
-                .setPenalizeNl(true)
-//                .setNProbs(10)
-                .setMirostat(InferenceParameters.MiroStat.V2)
-                .setAntiPrompt("User:");
-        String modelName = System.getProperty("model.name");
-        String modelPath = ModelResolver.getPathToModel(modelName);
         String system = "This is a conversation between User and Llama, a friendly chatbot.\n" +
                 "Llama is helpful, kind, honest, good at writing, and never fails to answer any " +
                 "requests immediately and with precision.\n\n" +
                 "User: Hello Llama\n" +
                 "Llama: Hello.  How may I help you today?";
-                ;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-        try (LlamaModel model = new LlamaModel(modelPath, modelParams)) {
+        try (LlamaModel model = new LlamaModel(modelParams)) {
             System.out.print(system);
             String prompt = system;
             while (true) {
@@ -41,10 +34,12 @@ public class MainExample {
                 prompt += input;
                 System.out.print("Llama: ");
                 prompt += "\nLlama: ";
-//                String answer = model.complete(prompt, inferParams);
-//                System.out.print(answer);
-//                prompt += answer;
-                for (LlamaModel.Output output : model.generate(prompt, inferParams)) {
+				InferenceParameters inferParams = new InferenceParameters(prompt)
+						.setTemperature(0.7f)
+						.setPenalizeNl(true)
+						.setMiroStat(MiroStat.V2)
+						.setStopStrings("User:");
+                for (LlamaModel.Output output : model.generate(inferParams)) {
                     System.out.print(output);
                     prompt += output;
                 }
