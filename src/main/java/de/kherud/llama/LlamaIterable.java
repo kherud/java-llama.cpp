442ba0b79c2cc80bb89a8e5964c5365cd5acb189
Konstantin Herud <konstantin.herud@gmail.com>
2024-07-24 23:00:46 +0200

    remove jetbrains annotations

--- src/main/java/de/kherud/llama/LlamaIterable.java
+++ src/main/java/de/kherud/llama/LlamaIterable.java
@@ -1,14 +1,11 @@
 package de.kherud.llama;
 
-import org.jetbrains.annotations.NotNull;
-
 /**
  * An iterable used by {@link LlamaModel#generate(InferenceParameters)} that specifically returns a {@link LlamaIterator}.
  */
 @FunctionalInterface
 public interface LlamaIterable extends Iterable<LlamaOutput> {
 
-    @NotNull
     @Override
     LlamaIterator iterator();
 
