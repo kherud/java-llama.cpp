95819f5e5a4ac105e1101cc9e53d35df376323c3
Grebennikov Roman <grv@dfdx.me>
2024-09-03 19:29:34 +0200

    use resources_linux_cuda as custom res name

--- src/main/java/de/kherud/llama/LlamaLoader.java
+++ src/main/java/de/kherud/llama/LlamaLoader.java
@@ -166,7 +166,7 @@ class LlamaLoader {
 	 * @param path path of the native library
 	 * @return true for successfully loading, otherwise false
 	 */
-	public static boolean loadNativeLibrary(Path path) {
+	private static boolean loadNativeLibrary(Path path) {
 		if (!Files.exists(path)) {
 			return false;
 		}
