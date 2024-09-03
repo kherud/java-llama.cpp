6530b799bf4c9db08a4799eff7f0e234d0d5cae7
Grebennikov Roman <grv@dfdx.me>
2024-09-03 21:56:43 +0200

    remove dockcross-linux-amd64-lts from update.sh

--- .github/dockcross/update.sh
+++ .github/dockcross/update.sh
@@ -5,7 +5,6 @@ docker run --rm dockcross/manylinux2014-x64  > ./dockcross-manylinux2014-x64
 docker run --rm dockcross/manylinux_2_28-x64  > ./dockcross-manylinux_2_28-x64
 docker run --rm dockcross/manylinux2014-x86  > ./dockcross-manylinux2014-x86
 docker run --rm dockcross/linux-arm64-lts    > ./dockcross-linux-arm64-lts
-docker run --rm dockcross/linux-amd64-lts    > ./dockcross-linux-amd64-lts
 docker run --rm dockcross/android-arm        > ./dockcross-android-arm
 docker run --rm dockcross/android-arm64      > ./dockcross-android-arm64
 docker run --rm dockcross/android-x86        > ./dockcross-android-x86
