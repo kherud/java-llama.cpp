027f2b66c6162adee49254f569202db07173702a
Konstantin Herud <konstantin.herud@gmail.com>
2025-02-03 22:57:25 +0100

    update config param enums

--- src/main/java/de/kherud/llama/args/PoolingType.java
+++ src/main/java/de/kherud/llama/args/PoolingType.java
@@ -2,7 +2,20 @@ package de.kherud.llama.args;
 
 public enum PoolingType {
 
-	UNSPECIFIED,
-	MEAN,
-	CLS
+    UNSPECIFIED(-1),
+    NONE(0),
+    MEAN(1),
+    CLS(2),
+    LAST(3),
+    RANK(4);
+
+    private final int id;
+
+    PoolingType(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
